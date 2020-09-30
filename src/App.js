ceaf1bbbb122a9955b0e3a87ea4fddf1ee321abc
unknown <tompsherman@gmail.com>
2020-09-30 10:17:20 -0400

    created project card, imported to App

--- src/App.js
+++ src/App.js
@@ -1,40 +1,12 @@
 import React from 'react'
 
 import './App.css'
+import ProjectCard from './components/ProjectCard'
 
 const App = () => {
   return (
     <div className="App">
-      {/* Remove everything from this line to line 37 */}
-      <h2>Welcome Lambda student! This is filler, no need to keep it!</h2>
-      <a href="https://lambdaschool.com/" target="_blank" rel="noopener noreferrer">
-        Visit the Lambda Llama home
-      </a>
-      <br />
-      <img
-        alt="Lambda loves our llamas!"
-        style={{ width: 'auto', height: '300px' }}
-        src="https://pbs.twimg.com/media/Egh41YNU4AADnsM.png"
-      ></img>
-      <div>
-        <p>Created using Create-React-App</p>
-        <p>Modified and maintained by Lambda School graduate:</p>
-        <p>
-          Zac Smith {' '}
-          <a href="https://github.com/mrzacsmith" target="_blank" rel="noopener noreferrer">
-            Github
-          </a>{' '}
-          ::{' '}
-          <a href="https://www.npmjs.com/~mrzacsmith" target="_blank" rel="noopener noreferrer">
-            NPM
-          </a>{' '}
-          ::{' '}
-          <a href="https://twitter.com/mrzacsmith" target="_blank" rel="noopener noreferrer">
-            Twitter
-          </a>
-        </p>
-      </div>
-      {/* Remove everything from line 8 to here */}
+      <ProjectCard />
     </div>
   )
 }
---
+++ src/components/ProjectCard.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ProjectCard = () => {
+    return (
+        <div>
+            {/* map over project db */}
+            <h2>Project Title</h2>
+            <h3>Project Tagline</h3>
+            <p>Project Description</p>
+            <a href="#">Project Link</a>
+            <img src="#" alt="alt" />
+        </div>
+    )
+}
+
+export default ProjectCard
