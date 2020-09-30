de7a88376609225f6854ea412b4743397a9be9ef
unknown <tompsherman@gmail.com>
2020-09-30 10:19:10 -0400

    added h1 to app

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import ProjectCard from './components/ProjectCard'
 const App = () => {
   return (
     <div className="App">
+      <h1>Tom Sherman's portfolio page:</h1>
       <ProjectCard />
     </div>
   )
