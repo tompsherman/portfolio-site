ec8c980dfd0b9aaed2c06628bfbf30949b4cd187
unknown <tompsherman@gmail.com>
2020-10-01 10:14:37 -0400

    project card styling, added styled-components

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,22 +1,33 @@
 import React from 'react'
+import styled from "styled-components"
 
 const ProjectCard = (props) => {
     return (
-        <div>
+        <StyledDiv>
             {props.data.map(project=>{
                 return(
                    <>
-                        <h2>{project.title}</h2>
-                        <h3>{project.tagline}</h3>
-                        <p>{project.description}</p>
-                        <a href={project.url}>{project.url}</a>
-                        <img src="#" alt="alt" />
+                        <div class ="project">
+                            <h2>{project.title}</h2>
+                            <h3>{project.tagline}</h3>
+                            <p>{project.description}</p>
+                            <a href={project.url}>{project.url}</a>
+                            <img src="#" alt="alt" />
+                        </div>
                    </>
                 )
             })}
             
-        </div>
+        </StyledDiv>
     )
 }
+
+const StyledDiv = styled.div`
+
+    .project{
+        width:30%;
+        border: 1px solid black;
+    }
+`
 
 export default ProjectCard
