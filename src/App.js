febdbf735eb81e42cdbd1f9c73965438398b32ad
unknown <tompsherman@gmail.com>
2020-10-02 18:25:27 -0400

    imported webdev card component to app, updated project card info

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React from 'react'
+import React from "react";
 
-import './App.css'
-import ProjectCard from './components/ProjectCard'
-import data from "./components/data"
+import "./App.css";
+import ProjectCard from "./components/ProjectCard";
+import projdata from "./components/projdata";
+import WebDevCard from "./components/WebDevCard";
+import webdata from "./components/webdata";
 
 const App = () => {
   return (
     <div className="App">
       <h1>Tom Sherman's portfolio page:</h1>
-      <ProjectCard data={data}/>
+      <WebDevCard webdata={webdata} />
+      <ProjectCard projdata={projdata} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
