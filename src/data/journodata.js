3fd42d60ee396cb0bbf5006612c993c29f8f85e5
unknown <tompsherman@gmail.com>
2020-10-03 10:12:01 -0400

    data folder added

---
+++ src/data/journodata.js
@@ -0,0 +1,51 @@
+export default [
+  {
+    title: "The Harlem Times",
+    tagline: "the race to build Africa's first exportable car",
+    description: "",
+    url: "",
+    image: "",
+  },
+  {
+    title: "92.7 WGMD",
+    tagline: "News Anchor",
+    description: "Voted 'Best of Delaware' for 2018 radio news downstate",
+    url: "",
+    image: "",
+  },
+  {
+    title: "VICE - Munchies",
+    tagline: "No Roadkill at the Roadkill Cookoff",
+    description: `Very disappointing.`,
+    url: "",
+    image: "",
+  },
+  {
+    title: "World HQ",
+    tagline: "",
+    description: "site based on award winning news coverage pioneered at WGMD",
+    url: "",
+    image: "",
+  },
+  {
+    title: "Cover 32",
+    tagline: " ",
+    description: "another unit 2 project",
+    url: "https://www.mooncatcomedy.com",
+    image: "",
+  },
+  {
+    title: "KPR Industries",
+    tagline: "Was it a Russian troll farm? we'll never know",
+    description: "",
+    url: "",
+    image: "",
+  },
+  {
+    title: "",
+    tagline: "",
+    description: "",
+    url: "",
+    image: "",
+  },
+];
---
+++ src/data/webdata.js
@@ -0,0 +1,53 @@
+export default [
+  {
+    title: "Unit 1 - UI / Marketing",
+    tagline: "",
+    description: "Did user interface web development.",
+    url: "https://www.thenewcalendar.com",
+    image: "",
+  },
+  {
+    title: "Unit 1 - Javascript",
+    tagline: "",
+    description:
+      "created a vanilla javscript site for the New Calendar, officially taking it into the digital realm",
+    url: "https://www.amazon.com/U-S-History-Joke-Billy-Peden/dp/B087629N8S",
+    image: "",
+  },
+  {
+    title: "Unit 1 build week",
+    tagline: "Sleep Tracker",
+    description: `Solo designer for the Sleep Tracker App, an App that lets you track your sleep.`,
+    url: "",
+    image: "",
+  },
+  {
+    title: "Unit 2 - rick and morty cards",
+    tagline: "We got a big f*cking problem, Morty",
+    description: "API get calls to create character cards",
+    url: "",
+    image: "",
+  },
+  {
+    title: "Unit 2 - another project or sprint",
+    tagline: " ",
+    description: "another unit 2 project",
+    url: "https://www.mooncatcomedy.com",
+    image: "",
+  },
+  {
+    title: "Unit 2 build week",
+    tagline: "Ex-pat Journal",
+    description:
+      "Exciting project that blended my love for travel with my penchant for forgetfulness. created an app that lets you document and create cards for events / memories and travels.",
+    url: "",
+    image: "",
+  },
+  {
+    title: "",
+    tagline: "",
+    description: "",
+    url: "",
+    image: "",
+  },
+];
