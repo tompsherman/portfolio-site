de636c52212e8a243018c7533eb60bf0b3368f60
unknown <tompsherman@gmail.com>
2020-10-03 10:12:42 -0400

    App streamlined

--- src/App.js
+++ src/App.js
@@ -2,18 +2,17 @@ import React from "react";
 
 import "./App.css";
 import ProjectCard from "./components/ProjectCard";
-import projdata from "./components/projdata";
-import WebDevCard from "./components/WebDevCard";
-import webdata from "./components/webdata";
-import JournoCard from "./components/JournoCard";
-import journodata from "./components/journodata";
+
+import projdata from "./data/projdata";
+import webdata from "./data/webdata";
+import journodata from "./data/journodata";
 
 const App = () => {
   return (
     <div className="App">
       <h1>Tom Sherman's portfolio page:</h1>
-      <JournoCard journodata={journodata} />
-      <WebDevCard webdata={webdata} />
+      <ProjectCard projdata={journodata} />
+      <ProjectCard projdata={webdata} />
       <ProjectCard projdata={projdata} />
     </div>
   );
