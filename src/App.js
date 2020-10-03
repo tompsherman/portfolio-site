d296132e13a78c798911b566c415721167605534
unknown <tompsherman@gmail.com>
2020-10-03 09:28:03 -0400

    journoCard on App

--- src/App.js
+++ src/App.js
@@ -5,11 +5,14 @@ import ProjectCard from "./components/ProjectCard";
 import projdata from "./components/projdata";
 import WebDevCard from "./components/WebDevCard";
 import webdata from "./components/webdata";
+import JournoCard from "./components/JournoCard";
+import journodata from "./components/journodata";
 
 const App = () => {
   return (
     <div className="App">
       <h1>Tom Sherman's portfolio page:</h1>
+      <JournoCard journodata={journodata} />
       <WebDevCard webdata={webdata} />
       <ProjectCard projdata={projdata} />
     </div>
