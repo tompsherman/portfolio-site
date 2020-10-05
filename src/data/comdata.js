ccdd59bc8c0432d900796afa43bd4c630fc38e41
unknown <tompsherman@gmail.com>
2020-10-05 06:22:50 -0400

    added comedy data

---
+++ src/data/comdata.js
@@ -0,0 +1,31 @@
+export default [
+  {
+    title: "Stand-up comedy",
+    tagline: "",
+    description: "performed across America, from New Mexico to New York",
+    url: "",
+    image: "",
+  },
+  {
+    title: "MoonCat Comedy Club",
+    tagline: "",
+    description: "founder of MoonCat Comedy, a 501(c)3 non-profit org.",
+    url: "",
+    image: "",
+  },
+  {
+    title: "History is a Joke",
+    tagline: "the world's first & funniest comedy textbook",
+    description: "factually funny jokes",
+    url: "",
+    image: "",
+  },
+  {
+    title: "the New Calendar",
+    tagline: "",
+    description:
+      "trying to convert the world from the gregorian calendat to a new calendar system seems like the absurd",
+    url: "",
+    image: "",
+  },
+];
